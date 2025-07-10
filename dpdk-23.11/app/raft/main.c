52fccced344552688b7162e4dbc4a08f5c33cfa5
SuQichen777 <hdredbean123123123@outlook.com>
2025-07-10 14:50:10 +0800

    add see metadata to raft

--- dpdk-23.11/app/raft/main.c
+++ dpdk-23.11/app/raft/main.c
@@ -5,35 +5,55 @@
 #include <rte_launch.h>
 #include <rte_lcore.h>
 #include <rte_timer.h>
+#include <rte_pause.h>	
 #include "election.h"
 #include "networking.h"
 #include "packet.h"
 #include "config.h"
+#include "metadata.h"
 
 // current time
 static uint64_t get_time_ms(void) {
     return rte_get_timer_cycles() * 1000 / rte_get_timer_hz();
 }
 
+static void
+stats_timer_cb(__rte_unused struct rte_timer *tim, void *arg)
+{
+    struct stats_lcore_params *st = arg;
+    print_stats(st);
+}
+
+
 // DPDK work thread main function
-static int lcore_main(__attribute__((unused)) void *arg) {
+static int lcore_main(void *arg)
+{
+    struct stats_lcore_params *st = arg;
+    static struct rte_timer   stats_timer;
+    static int timer_init_done = 0;
+
+    if (!timer_init_done) {
+        uint64_t hz = rte_get_timer_hz();
+        rte_timer_init(&stats_timer);
+        rte_timer_reset(&stats_timer, hz * 10, PERIODICAL,
+                        rte_lcore_id(), stats_timer_cb, st);
+        timer_init_done = 1;
+    }
+
     uint64_t last_heartbeat = 0;
     printf("lcore_main running on lcore %u\n", rte_lcore_id());
-    while (1) {
-        // looping
 
+    for (;;) {
         process_packets();
         rte_timer_manage();
-        // send heartbeat if this node is the leader
+
         if (raft_get_state() == STATE_LEADER) {
-            uint64_t now = get_time_ms();
+            uint64_t now = rte_get_timer_cycles() * 1000 / rte_get_timer_hz();
             if (now - last_heartbeat >= 500) {
                 raft_send_heartbeat();
                 last_heartbeat = now;
             }
         }
-        
-        // delay using DPDK's timer
         rte_pause();
     }
     return 0;
@@ -54,10 +74,13 @@ int main(int argc, char **argv) {
     rte_timer_subsystem_init();
     net_init();
     raft_init(global_config.node_id);
+    struct app_config_params app = { .port_id = global_config.port_id };
+    struct stats_lcore_params st = { .app_params = &app };
+    print_stats(&st);
     
     printf("Node %u starting...\n", raft_get_node_id());
     
-    rte_eal_mp_remote_launch(lcore_main, NULL, CALL_MAIN);
+    rte_eal_mp_remote_launch(lcore_main, &st, CALL_MAIN);
     rte_eal_mp_wait_lcore();
     
     return 0;
