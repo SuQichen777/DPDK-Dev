4aada194130f9b1bc33ef2b6b0e75b9b4d0d5117
SuQichen777 <hdredbean123123123@outlook.com>
2025-07-15 16:24:44 +0800

    fix: rename latency_timer to ps_timer and update callback for PS broadcast sending

--- dpdk-23.11/app/raft-netelect/main.c
+++ dpdk-23.11/app/raft-netelect/main.c
@@ -31,7 +31,7 @@ static int lcore_main(void *arg)
 {
     struct stats_lcore_params *st = arg;
     static struct rte_timer   stats_timer;
-    static struct rte_timer latency_timer;
+    static struct rte_timer ps_timer;
     static int timer_init_done = 0;
 
     if (!timer_init_done) {
@@ -39,10 +39,10 @@ static int lcore_main(void *arg)
         rte_timer_init(&stats_timer);
         rte_timer_reset(&stats_timer, hz * 10, PERIODICAL,
                         rte_lcore_id(), stats_timer_cb, st);
-        rte_timer_init(&latency_timer);
-        rte_timer_reset(&latency_timer, hz / 200, PERIODICAL,
+        rte_timer_init(&ps_timer);
+        rte_timer_reset(&ps_timer, hz / 200, PERIODICAL,
                         rte_lcore_id(),
-                        (rte_timer_cb_t)latency_poll_once,
+                        (rte_timer_cb_t)ps_broadcast_send,
                         (void *)(uintptr_t)global_config.port_id);
         timer_init_done = 1;
     }
