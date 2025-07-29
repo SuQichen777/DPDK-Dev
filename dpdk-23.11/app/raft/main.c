cf215a6ddcaaf4c9b40b7bb337f76d9dfe0449d9
SuQichen777 <hdredbean123123123@outlook.com>
2025-07-29 17:55:36 +0800

    fix: initialize stats_lcore_params structure in main function

--- dpdk-23.11/app/raft/main.c
+++ dpdk-23.11/app/raft/main.c
@@ -85,6 +85,7 @@ int main(int argc, char **argv) {
     net_init();
     raft_init(global_config.node_id);
     struct app_config_params app = { .port_id = global_config.port_id };
+    struct stats_lcore_params st = { .app_params = &app };
     // struct stats_lcore_params st = { .app_params = &app };
     // print_stats(&st);
     
